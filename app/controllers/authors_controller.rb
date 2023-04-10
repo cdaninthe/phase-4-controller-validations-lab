19e413ec15e289b8ececc8301aecfc4a38c002b4
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-04-10 14:39:22 -0400

    commit tree c7f0e99412b9e8002a9e4f87f3138da09704aafe

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
   def show
     author = Author.find(params[:id])
@@ -7,7 +9,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
+    author = Author.create!(author_params)
 
     render json: author, status: :created
   end
@@ -18,4 +20,13 @@ class AuthorsController < ApplicationController
     params.permit(:email, :name)
   end
   
+  def render_not_found_response
+    render json: {errors: ["Post was not found."]}, status: not_found 
+  end
+
+  def render_unprocessable_entity_response(author)
+    render json: { errors: author.record.errors }, status: :unprocessable_entity
+  end
+
+
 end
